2160b236e1a423ccf42797e14b38fb30d7839a4d
IrinaMedinaSierra <irina@ceatformacion.com>
2024-08-14 12:07:42 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,48 @@
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Adidas Shop</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <div class="header">
+        <a href="login.php">Iniciar Sesión</a>
+    </div>
+    <nav>
+        <img src="media/logo-adidas.webp" alt="Logo Adidas">
+<!--        menu-->
+        <div class="menu">
+            <a href="#mujer">Mujer</a>
+            <a href="#hombre">Hombre</a>
+            <a href="#infantil">Infantil</a>
+            <a href="#zapatillas">Zapatillas</a>
+            <a href="#contacto">Contacto</a>
+        </div>
+
+    </nav>
+
+<div id="mujer">
+
+</div>
+
+<div id="hombre">
+
+</div>
+<div id="infantil">
+
+</div>
+<div id="zapatillas">
+
+</div>
+<div id="contacto">
+
+</div>
+
+</body>
+</html>
